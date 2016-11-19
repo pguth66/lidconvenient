9083606f8711f4232d1f041d18e5053f090b85a2
g <gunther@cloudscaling.com>
2016-11-19 12:22:39 -0800

    basic functionality

---
+++ js/lidconvenient.js
@@ -0,0 +1,158 @@
+function runLU ( ) {
+
+	var runs = 1000 ;
+	var lastState = 0 ; // 0 = lid down, 1 = seat down, 2 = seat up
+	var isMan = false ;
+	var currentRun = 0 ;
+	var numInconv = 0 ;
+	const numberTwoPercent = 0.2 ;
+	var isNumberTwo = false ;
+	var ranNum = 0 ;
+
+	for ( i = 1 ; i < runs ; i++ ) {
+		// assume 50% chance it's a man or a woman
+		if ( Math.random() > 0.6 ) {
+			isMan = true ;
+		}
+		else {
+			isMan = false ; 
+		}
+
+		if ( Math.random() < numberTwoPercent ) {
+			isNumberTwo = true ; 
+		}
+		else {
+			isNumberTwo = false ;
+		}
+
+		if ( lastState == 0 ) {
+			numInconv++ ;
+			console.log("Lid was down");
+		}
+		else if ( ( lastState == 1 ) && isMan && !isNumberTwo ) {
+			numInconv++ ;
+			lastState = 2 ;
+		}
+		else if ( ( lastState == 2  ) && (!isMan) ) {
+			numInconv++ ;
+			console.log("seat was up and it's not a man");
+			lastState = 1 ;
+		}
+		else if ( ( lastState == 2 ) && isMan && isNumberTwo ) {
+			numInconv++ ;
+		}	
+
+		if ( isMan && !isNumberTwo ) {
+			lastState = 2 ; 
+		}
+		else {
+			lastState = 1 ;
+		}
+
+		lastState = 2 ; //always put the seat up
+
+		document.getElementById("col3runs").innerHTML =  i ;
+		document.getElementById("col3inconv").innerHTML = numInconv ;
+		document.getElementById("col3inconvpercent").innerHTML = (numInconv / i).toFixed(2) * 100 ;
+	}
+}
+function runLIR ( ) {
+
+	var runs = 1000 ;
+	var lastState = 0 ; // 0 = lid down, 1 = seat down, 2 = seat up
+	var isMan = false ;
+	var currentRun = 0 ;
+	var numInconv = 0 ;
+	const numberTwoPercent = 0.2 ;
+	var isNumberTwo = false ;
+	var ranNum = 0 ;
+
+	for ( i = 1 ; i < runs ; i++ ) {
+		// assume 50% chance it's a man or a woman
+		if ( Math.random() > 0.6 ) {
+			isMan = true ;
+		}
+		else {
+			isMan = false ; 
+		}
+
+		if ( Math.random() < numberTwoPercent ) {
+			isNumberTwo = true ; 
+		}
+		else {
+			isNumberTwo = false ;
+		}
+
+		if ( lastState == 0 ) {
+			numInconv++ ;
+			console.log("Lid was down");
+			if ( isMan && !isNumberTwo ) {
+				lastState = 2 ; 
+			}
+			else {
+				lastState = 1 ;
+			}
+		}
+		else if ( ( lastState == 1 ) && isMan && !isNumberTwo ) {
+			numInconv++ ;
+			lastState = 2 ;
+		}
+		else if ( ( lastState == 2 ) && (!isMan) ) {
+			numInconv++ ;
+			console.log("seat was up and it's a man");
+			lastState = 1 ;
+		}
+
+		document.getElementById("col2runs").innerHTML =  i ;
+		document.getElementById("col2inconv").innerHTML = numInconv ;
+		document.getElementById("col2inconvpercent").innerHTML = (numInconv / i).toFixed(2) * 100 ;
+	}
+}
+
+function runLUSD ( ) {
+
+	var runs = 1000 ;
+	var lastState = 0 ; // 0 = lid down, 1 = seat down, 2 = seat up
+	var isMan = false ;
+	var currentRun = 0 ;
+	var numInconv = 0 ;
+	const numberTwoPercent = 0.2 ;
+	var isNumberTwo = false ;
+	var ranNum = 0 ;
+
+	for ( i = 1 ; i < runs ; i++ ) {
+		// assume 50% chance it's a man or a woman
+		if ( Math.random() > 0.6 ) {
+			isMan = true ;
+		}
+		else {
+			isMan = false ; 
+		}
+
+		if ( Math.random() < numberTwoPercent ) {
+			isNumberTwo = true ; 
+		}
+		else {
+			isNumberTwo = false ;
+		}
+
+		if ( lastState == 0 ) {
+			numInconv++ ;
+			console.log("Lid was down");
+		}
+		else if ( ( lastState == 1 ) && isMan && !isNumberTwo ) {
+			numInconv++ ;
+		}
+		else if ( ( lastState == 2 ) && (!isMan) ) {
+			numInconv++ ;
+			console.log("seat was up and it's a man");
+		}
+
+		lastState = 1 ; // always put the seat down
+
+		document.getElementById("col1runs").innerHTML =  i ;
+		document.getElementById("col1inconv").innerHTML = numInconv ;
+		document.getElementById("col1inconvpercent").innerHTML = (numInconv / i).toFixed(2) * 100 ;
+	}
+}
+
